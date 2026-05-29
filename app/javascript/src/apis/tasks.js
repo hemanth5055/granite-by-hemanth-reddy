92cdf652729603b8f7e5f85ae08c4bb0b37f6781
Kanala Hemanth Reddy <kanalahemanthrdy@gmail.com>
2026-05-29 12:36:46 +0530

    Implemented create action

--- app/javascript/src/apis/tasks.js
+++ app/javascript/src/apis/tasks.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const fetch = () => axios.get("/tasks");
+const create = payload => axios.post("/tasks", { task: payload });
 
-const tasksApi = { fetch };
+const tasksApi = { fetch, create };
 
 export default tasksApi;
